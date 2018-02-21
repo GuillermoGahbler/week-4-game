05dc2eb0d71c14674e56c30c397094aff757e81f
GuillermoGahbler <mexamericano@gmail.com>
2018-02-21 15:18:13 -0800

    updates made to index.html style.css game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+}) //this may need to wrap around all the js
+ 
+    gems = ['assets/images/amethys.png', 'assets/images/aqua.png','assets/images/emerald.png', 'assets/images/zircon.png'];
+
+    var counter = 0;
+    var wins = 0;
+    var losses = 0;
+
+    $('#win').text(wins);
+    $('#loss').text(losses);
+    
+    
