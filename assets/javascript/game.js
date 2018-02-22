80f90bb256e38552d96609d17dc74048d0fdcd20
GuillermoGahbler <mexamericano@gmail.com>
2018-02-21 21:32:49 -0800

    changes made to index.html style.css game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// pseudo code
+// assign each crystal a random number between 1-12 at the begining of each game 
+// a random number between 19-120 generates at the begining of each game
+// each crystal click will reveal the random number that was generated for that crystal, subsequent clicks will add the new number revealed to the last number generated when clicked
+// if all crystal numbers added up to equal the random number generated the  player wins, the total wins is updated by 1 and the game starts over.
+// if all the crytals add up higher than random number generated the player loses and game the total losses is updated by 1 and the game starts over.
+// when game starts new crystal numbers will be randomly generated (hidden) also a new random number will be generated
+
+
+var randomNum;
+var wins;
+var losses;
+
